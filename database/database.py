b07c9ce25288194bbcd4f8ebd695a171960b18bc
joannanuzka <64213022+joannanuzka@users.noreply.github.com>
2023-11-10 15:38:20 +0100

    Add function to get password

--- database/database.py
+++ database/database.py
@@ -3,12 +3,20 @@ from typing import List, Dict
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, insert
 from sqlalchemy.schema import MetaData
+from sqlalchemy.sql import text
 import os
 
 
 def insert_to_table(db_conn, metadata: MetaData, tablename: str, insert_data_lst: List[Dict[str, str]]):
     for data in insert_data_lst:
         db_conn.execute(metadata.tables[tablename].insert(data))
+
+
+def get_password(db_conn, username: str) -> str:
+    query = "Select password from User where username = '" + username + "'"
+    res = db_conn.execute(query)
+    for r in res:
+        return r[0]
 
 
 load_dotenv()
@@ -19,4 +27,8 @@ db_con = eng.connect()
 
 meta = MetaData()
 meta.reflect(bind=eng)
+
+
+
+
 
