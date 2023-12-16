80d84221b42364e66bfecdfe00e655ec80d753db
joannanuzka <64213022+joannanuzka@users.noreply.github.com>
2023-12-16 11:33:26 +0100

    DB changes

--- database/database_definitions.py
+++ database/database_definitions.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.schema import MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 import os
 
 
@@ -19,7 +20,30 @@ meta.reflect(bind=eng)
 Base = automap_base()
 Base.prepare(eng, reflect=True)
 
-User = Base.classes.User
+Base1 = declarative_base()
+
+class User(Base1):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
+    username = Column(String(30), nullable = False)
+    password = Column(String(180), nullable = False)
+    email = Column(String(30), nullable = False)
+    createdon = Column(DateTime, nullable = False)
+    createdby = Column(String(15), nullable = False)
+    lastupdatedon = Column(DateTime, nullable = False)
+    lastupdatedby = Column(String(15), nullable = False)
+    active = Column(Boolean, nullable = False)
+
+    is_authenticated = False
+    is_anonymous = False
+    is_active = active
+
+    def get_id(self):
+        return str(self.id)
+
+
+# User = Base.classes.User
 Role = Base.classes.Role
 UserRole = Base.classes.UserRole
 UserVote = Base.classes.UserVote
