2f7ed83e56466d6bc793b6e0d41f92396fa31d80
Motyleczek <micha.motyl24@gmail.com>
2024-01-10 13:57:54 +0100

    pierwsze pobieranie danych

--- website/views.py
+++ website/views.py
@@ -10,6 +10,9 @@ import datetime
 import pytz
 import string
 
+import database
+
+from typing import List
 
 views = Blueprint('views', __name__)
 
@@ -17,26 +20,6 @@ ALLOWED_EXTENSIONS = {'jpg', 'png'}
 
 @views.route('/', methods=["GET", "POST"])
 def index():
-    # conn = None
-    # data = None
-    # try:
-    #     params = config()
-    #     n = random.randint(1, 3226)
-    #     conn = psycopg2.connect(**params)
-
-    #     print('Connected succesfully')
-    #     cur = conn.cursor()
-
-    #     data = cur.fetchall()
-
-    # except(Exception, psycopg2.Error) as error:
-    #     print(error)
-
-    # finally:
-    #     if conn is not None:
-    #         cur.close()
-    #         conn.close()
-
     return render_template('index.html')
 
 @views.route("/quadratic_descr", methods=["GET", "POST"])
@@ -46,4 +29,27 @@ def quadratic_descr():
 
 @views.route("/vote_creation", methods=["GET", "POST"])
 def vote_creation():
+    title : str = None
+    data_start : datetime = None
+    data_end : datetime = None
+    vote_type : str = None # TODO: mozna by to przerobic na enuma
+    num_credits : int = None
+    vote_options: List[str] = []
+    
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       title = request.form.get("title")
+       # getting input with name = lname in HTML form 
+       data_start = request.form.get("data_start")
+       data_end = request.form.get("data_end")
+       vote_type = request.form.get("vote_type")
+       num_credits = request.form.get("credits")
+       for key in request.form.keys():
+           if len(key) >= 6:
+            if key[:5] == 'input':
+                    vote_option_x = request.form.get(key)
+                    vote_options.append(vote_option_x)
+       
+       
+    
     return render_template("vote_creation.html")
